8e1862a8ffe14bfca85add0e5b0f177f3cad4d58
Trapland <89914789+Trapland@users.noreply.github.com>
2022-09-09 17:58:05 +0200

    Добавьте файлы проекта.

---
+++ ContextMenu/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ContextMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.createButtonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createCheckBoxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createButtonToolStripMenuItem,
+            this.createToolStripMenuItem,
+            this.createCheckBoxToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(211, 100);
+            // 
+            // createButtonToolStripMenuItem
+            // 
+            this.createButtonToolStripMenuItem.Name = "createButtonToolStripMenuItem";
+            this.createButtonToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.createButtonToolStripMenuItem.Text = "CreateButton";
+            this.createButtonToolStripMenuItem.Click += new System.EventHandler(this.createButtonToolStripMenuItem_Click);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.createToolStripMenuItem.Text = "CreateTextBox";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
+            // createCheckBoxToolStripMenuItem
+            // 
+            this.createCheckBoxToolStripMenuItem.Name = "createCheckBoxToolStripMenuItem";
+            this.createCheckBoxToolStripMenuItem.Size = new System.Drawing.Size(240, 32);
+            this.createCheckBoxToolStripMenuItem.Text = "CreateCheckBox";
+            this.createCheckBoxToolStripMenuItem.Click += new System.EventHandler(this.createCheckBoxToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem createButtonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createCheckBoxToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ ContextMenu/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ContextMenu
+{
+    public partial class Form1 : Form
+    {
+        int choice = 0;
+        int count_but = 3;
+        int count_text = 3;
+        int count_check = 3;
+        string but_text;
+        string text_text;
+        string check_text;
+        public Form1()
+        {
+            InitializeComponent();
+            but_text = createButtonToolStripMenuItem.Text;
+            text_text = createToolStripMenuItem.Text;
+            check_text = createCheckBoxToolStripMenuItem.Text;
+            createButtonToolStripMenuItem.Text = but_text + count_but.ToString();
+            createToolStripMenuItem.Text = text_text + count_text.ToString();
+            createCheckBoxToolStripMenuItem.Text = check_text + count_check.ToString();
+            menuStrip1.Items.Add(createButtonToolStripMenuItem);
+            menuStrip1.Items.Add(createToolStripMenuItem);
+            menuStrip1.Items.Add(createCheckBoxToolStripMenuItem);
+        }
+
+        private void createButtonToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            choice = 1;
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            choice = 2;
+        }
+
+        private void createCheckBoxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            choice = 3;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (choice == 1)
+            {
+                if (count_but > 0)
+                {
+                    Button button = new Button();
+                    button.Location = new Point(e.X, e.Y);
+                    button.Size = new Size(100, 100);
+                    this.Controls.Add(button);
+                    count_but--;
+                    createButtonToolStripMenuItem.Text = but_text + count_but.ToString();
+                }
+                if (count_but == 0)
+                {
+                    createButtonToolStripMenuItem.Enabled = false;
+                }
+            }
+            else if (choice == 2)
+            {
+                if (count_text > 0)
+                {
+                    TextBox text = new TextBox();
+                    text.Location = new Point(e.X, e.Y);
+                    text.Size = new Size(100, 100);
+                    this.Controls.Add(text);
+                    count_text--;
+                    createToolStripMenuItem.Text = text_text + count_text.ToString();
+                }
+                if (count_text == 0)
+                {
+                    createToolStripMenuItem.Enabled = false;
+                }
+            }
+            else if (choice == 3)
+            {
+                if (count_check > 0)
+                {
+                    CheckBox check = new CheckBox();
+                    check.Location = new Point(e.X, e.Y);
+                    check.Size = new Size(100, 100);
+                    this.Controls.Add(check);
+                    count_check--;
+                    createCheckBoxToolStripMenuItem.Text = check_text + count_check.ToString();
+                }
+                if (count_check == 0)
+                {
+                    createCheckBoxToolStripMenuItem.Enabled = false;
+                }
+            }
+        }
+    }
+}
